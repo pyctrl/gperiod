f86024815fc9d71f5bcfbb4499b6952a5f160c52
Dima Burmistrov <pyctrl.dev@gmail.com>
2023-01-30 23:16:14 +0300

    Add initial join implementation

--- period.py
+++ period.py
@@ -13,6 +13,7 @@ _T_DT_PAIR = tuple[datetime.datetime, datetime.datetime]
 #  - review exceptions
 #  - review Period class implementation
 #  - do performance tests
+#  - assert `start < end`
 
 
 class PeriodProto(t.Protocol):
@@ -49,11 +50,32 @@ class Period(PeriodProto):
     def as_dict(self) -> dict[str, datetime.datetime]:
         return dict(start=self.start, end=self.end)
 
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Period):  # TODO(d.burmistrov): questionable
+            return NotImplemented
+        return self.start == other.start and self.end == other.end
+
 
 def join(*periods: PeriodProto,
          flat: bool = False,
          ) -> Period | _T_DT_PAIR:
-    raise NotImplementedError()
+    if not periods:
+        raise TypeError("not enough periods")
+
+    result = [periods[0].start, periods[0].end]
+    for p in periods[1:]:
+        if p.end == result[0]:  # `p` on the left
+            result[0] = p.start
+        elif p.start == result[1]:  # `p` on the right
+            result[1] = p.end
+        else:
+            raise ValueError("periods must be consecutive")
+
+    if flat:
+        return t.cast(tuple[datetime.datetime, datetime.datetime],
+                      tuple(result))
+    else:
+        return Period(*result)  # type: ignore[abstract]
 
 
 def union(*periods: PeriodProto,
--- test_period.py
+++ test_period.py
@@ -6,6 +6,34 @@ import period
 
 FAKE_TS_01 = datetime.datetime(2019, 2, 1, 10, 0, 0)
 FAKE_TS_02 = datetime.datetime(2019, 4, 14, 10, 0)
+FAKE_TS_03 = datetime.datetime(2019, 5, 20, 10, 0)
+FAKE_TS_04 = datetime.datetime(2019, 6, 25, 10, 0)
+FAKE_TS_05 = datetime.datetime(2019, 7, 31, 10, 0)
+FAKE_TS_06 = datetime.datetime(2019, 9, 5, 10, 0)
+FAKE_TS_07 = datetime.datetime(2019, 10, 11, 10, 0)
+FAKE_TS_08 = datetime.datetime(2019, 11, 16, 10, 0)
+FAKE_TS_09 = datetime.datetime(2019, 12, 22, 10, 0)
+FAKE_TS_10 = datetime.datetime(2020, 1, 27, 10, 0)
+FAKE_TS_11 = datetime.datetime(2020, 3, 3, 10, 0)
+FAKE_TS_12 = datetime.datetime(2020, 4, 8, 10, 0)
+FAKE_TS_13 = datetime.datetime(2020, 5, 14, 10, 0)
+FAKE_TS_14 = datetime.datetime(2020, 6, 19, 10, 0)
+FAKE_TS_15 = datetime.datetime(2020, 7, 25, 10, 0)
+FAKE_TS_16 = datetime.datetime(2020, 8, 30, 10, 0)
+FAKE_TS_17 = datetime.datetime(2020, 10, 5, 10, 0)
+FAKE_TS_18 = datetime.datetime(2020, 11, 10, 10, 0)
+FAKE_TS_19 = datetime.datetime(2020, 12, 16, 10, 0)
+FAKE_TS_20 = datetime.datetime(2021, 1, 21, 10, 0)
+FAKE_TS_21 = datetime.datetime(2021, 2, 26, 10, 0)
+FAKE_TS_22 = datetime.datetime(2021, 4, 3, 10, 0)
+FAKE_TS_23 = datetime.datetime(2021, 5, 9, 10, 0)
+FAKE_TS_24 = datetime.datetime(2021, 6, 14, 10, 0)
+FAKE_TS_25 = datetime.datetime(2021, 7, 20, 10, 0)
+FAKE_TS_26 = datetime.datetime(2021, 8, 25, 10, 0)
+FAKE_TS_27 = datetime.datetime(2021, 9, 30, 10, 0)
+FAKE_TS_28 = datetime.datetime(2021, 11, 5, 10, 0)
+FAKE_TS_29 = datetime.datetime(2021, 12, 11, 10, 0)
+FAKE_TS_30 = datetime.datetime(2022, 1, 16, 10, 0)
 
 
 class PeriodTestCase(unittest.TestCase):
@@ -16,3 +44,61 @@ class PeriodTestCase(unittest.TestCase):
         self.assertIs(result.start, FAKE_TS_01)
         self.assertIs(result.end, FAKE_TS_02)
         self.assertIsInstance(result, period.Period)
+
+
+class JoinResultPeriodTestCase(unittest.TestCase):
+
+    def test_empty_args(self):
+        self.assertRaises(TypeError, period.join)
+
+    def test_single(self):
+        p = period.Period(FAKE_TS_01, FAKE_TS_02)
+
+        result = period.join(p)
+
+        self.assertIsInstance(result, period.Period)
+        self.assertEqual(result, p)
+        self.assertIsNot(result, p)
+
+    def test_multi(self):
+        p1 = period.Period(FAKE_TS_01, FAKE_TS_02)
+        p2 = period.Period(FAKE_TS_02, FAKE_TS_03)
+        p3 = period.Period(FAKE_TS_03, FAKE_TS_08)
+        expected = period.Period(FAKE_TS_01, FAKE_TS_08)
+
+        result = period.join(p2, p3, p1)
+
+        self.assertIsInstance(result, period.Period)
+        self.assertEqual(result, expected)
+
+
+class JoinResultDatetimeTestCase(unittest.TestCase):
+
+    def test_empty_args(self):
+        self.assertRaises(TypeError, period.join, return_datetime=True)
+
+    def test_single(self):
+        p = period.Period(FAKE_TS_01, FAKE_TS_02)
+
+        result = period.join(p, flat=True)
+
+        self.assertIsInstance(result, tuple)
+        self.assertEqual(len(result), 2)
+        self.assertIsInstance(result[0], datetime.datetime)
+        self.assertIsInstance(result[1], datetime.datetime)
+        self.assertEqual(result[0], FAKE_TS_01)
+        self.assertEqual(result[1], FAKE_TS_02)
+
+    def test_multi(self):
+        p1 = period.Period(FAKE_TS_01, FAKE_TS_02)
+        p2 = period.Period(FAKE_TS_02, FAKE_TS_03)
+        p3 = period.Period(FAKE_TS_03, FAKE_TS_08)
+
+        result = period.join(p2, p3, p1, flat=True)
+
+        self.assertIsInstance(result, tuple)
+        self.assertEqual(len(result), 2)
+        self.assertIsInstance(result[0], datetime.datetime)
+        self.assertIsInstance(result[1], datetime.datetime)
+        self.assertEqual(result[0], FAKE_TS_01)
+        self.assertEqual(result[1], FAKE_TS_08)
