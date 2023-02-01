365208efac010559dba5014e12bfc91cf2fb7fd2
Dima Burmistrov <pyctrl.dev@gmail.com>
2023-02-01 19:42:39 +0300

    Respect linters

--- period.py
+++ period.py
@@ -1,13 +1,15 @@
+from __future__ import annotations
 import datetime
 import typing as t
 
 
 _F_START = "start"
 _F_END = "end"
-_T_DT_PAIR = t.Tuple[datetime.datetime, datetime.datetime]
+_T_DT_PAIR = tuple[datetime.datetime, datetime.datetime]
 
 
 # TODO(d.burmistrov):
+#  - timezone support
 #  - review exceptions
 #  - review Period class implementation
 #  - do performance tests
@@ -37,29 +39,29 @@ class Period(PeriodProto):
 def join(*periods: PeriodProto,
          flat: bool = False,
          ) -> Period | _T_DT_PAIR:
-    pass
+    raise NotImplementedError()
 
 
 def union(*periods: PeriodProto,
           flat: bool = False,
           ) -> Period | _T_DT_PAIR:
-    pass
+    raise NotImplementedError()
 
 
 def split(p: PeriodProto,
           *datetimes: datetime.datetime,
           ) -> t.Generator[Period | _T_DT_PAIR, None, None]:
-    pass
+    raise NotImplementedError()
 
 
 def intersection(*periods: PeriodProto,
                  flat: bool = False
                  ) -> t.Optional[Period | _T_DT_PAIR]:
-    pass
+    raise NotImplementedError()
 
 
 def difference(p: PeriodProto,
                *periods: PeriodProto,
                flat: bool = False,
                ) -> t.Generator[Period | _T_DT_PAIR, None, None]:
-    pass
+    raise NotImplementedError()
